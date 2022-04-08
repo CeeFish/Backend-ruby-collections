6063e9a27bab654ea5306fca3b9da2e33b1551a4
CeeFish <can.o.fisher@gmail.com>
2022-04-08 14:48:48 -0400

    Sorted_words file running and working

--- sorted_words.rb
+++ sorted_words.rb
@@ -1,3 +1,18 @@
 #(2) Write a program called sorted_words.rb.  It should prompt the user for words and add each to an array.  
 #The user should be able to add as many words as they like, until they just hit enter to return a blank word. 
 #Then sort the array using the sort method and print it out.
+
+sorted_words = Array.new
+sorted_words = []
+
+loop do
+    print "Enter as many words as you would like."
+    answer = gets.chomp
+    if answer == ""
+        break
+    end
+    sorted_words.push answer
+end 
+
+print sorted_words.sort
+
